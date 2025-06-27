a2056cc845d61498f4886adf9719e5d95f3ec8dc
leonmwilambwe16 <leonmwilambwe16@gmail.com>
2025-06-27 14:24:45 -0400

    todAY

--- backend/server.js
+++ backend/server.js
@@ -40,8 +40,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
   })
 
-}
-
+};
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
