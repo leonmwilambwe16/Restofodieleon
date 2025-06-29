a46e35525580ae67ee92275addf06e39e8cc0b1c
leonmwilambwe16 <leonmwilambwe16@gmail.com>
2025-06-29 13:28:52 -0400

    mono change on its branche

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -10,6 +10,15 @@ export const useAuth =()=> useContext(AuthContext);
   baseURL: url,
   withCredentials: true, 
 });
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
 
 export const AuthProvider = ({children})=>{
   const [user, setUser] = useState(null); 
